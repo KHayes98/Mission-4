deb1be7797952bf7e5f65021737c2e9be40af723
Reboot <kchayes2016@gmail.com>
2023-01-31 20:34:08 -0700

    Updated javascript

--- Mission-4/Mission-4/wwwroot/js/gradeCalc.js
+++ Mission-4/Mission-4/wwwroot/js/gradeCalc.js
@@ -1,12 +1,59 @@
-﻿//variables to hold the input from the user
-var assignmentValue = parseInt($("#assignmentInput").value);
-var projectValue = parseInt($("#projectInput").value);
-var quizzesValue = parseInt($("#quizzesInput").value);
-var midtermValue = parseInt($("#midtermInput").value);
-var finalValue = parseInt($("#finalInput").value);
-var intexValue = parseInt($("#intexInput").value);
+﻿//function to hopefully make it all work
+function getInput() {
+    var assignmentValue = parseInt($("#assignmentInput").value);
+    var projectValue = parseInt($("#projectInput").value);
+    var quizzesValue = parseInt($("#quizzesInput").value);
+    var midtermValue = parseInt($("#midtermInput").value);
+    var finalValue = parseInt($("#finalInput").value);
+    var intexValue = parseInt($("#intexInput").value);
+
+    var finalGrade = assignmentValue + projectValue + quizzesValue + midtermValue + finalValue + intexValue;
+    var letterGrade = "";
+
+    //sequence to determine the letter grade
+    if (finalGrade >= 94) {
+        letterGrade = "A";
+    }
+    else if (finalGrade >= 90 && finalGrade < 94) {
+        letterGrade = "A-";
+    }
+    else if (finalGrade >= 87 && finalGrade < 90) {
+        letterGrade = "B+";
+    }
+    else if (finalGrade >= 84 && finalGrade < 87) {
+        letterGrade = "B";
+    }
+    else if (finalGrade >= 80 && finalGrade < 84) {
+        letterGrade = "B-";
+    }
+    else if (finalGrade >= 77 && finalGrade < 80) {
+        letterGrade = "C+";
+    }
+    else if (finalGrade >= 74 && finalGrade < 77) {
+        letterGrade = "C";
+    }
+    else if (finalGrade >= 70 && finalGrade < 74) {
+        letterGrade = "C-";
+    }
+    else if (finalGrade >= 67 && finalGrade < 70) {
+        letterGrade = "D+";
+    }
+    else if (finalGrade >= 64 && finalGrade < 67) {
+        letterGrade = "D";
+    }
+    else if (finalGrade >= 60 && finalGrade < 64) {
+        letterGrade = "D-";
+    }
+    else {
+        letterGrade = "E";
+    }
+
+    $("#gradeInsert").innerHTML = letterGrade;
+}
 
 
+/*
+ * 
 //button to submit grades and return a letter grade
 $("#submitGrade").click(function () {
     $("gradeInsert").innerHtml = letterGrade;
@@ -54,3 +101,5 @@ else if (finalGrade >= 60 && finalGrade < 64) {
 else {
     letterGrade = "E";
 }
+
+*/
